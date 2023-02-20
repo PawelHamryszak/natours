22389effa6aec5a381583eba13f2d76e8a390a9f
Paweł hamryszak <phamryszak91@gmail.com>
2023-02-20 15:57:50 +0100

    bundle update

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -37921,7 +37921,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "2884" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "3777" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
@@ -38066,4 +38066,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/bundle.js.map
+//# sourceMappingURL=/js/bundle.js.map
