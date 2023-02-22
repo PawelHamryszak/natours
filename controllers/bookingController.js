3d55905aa4ea33b36075598e0e3b991013137bfc
Paweł hamryszak <phamryszak91@gmail.com>
2023-02-22 15:52:03 +0100

    booking update 2

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -60,21 +60,22 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.amount_total / 100;
+  const price = session.display_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 };
 
 exports.webhookCheckout = (req, res, next) => {
-  const sig = req.headers['stripe-signature'];
+  const signature = req.headers['stripe-signature'];
+
   let event;
   try {
     event = stripe.webhooks.constructEvent(
       req.body,
-      sig,
-      process.env.WEBHOOK_SECRET
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET
     );
   } catch (err) {
-    return res.status(400).send(`Webhook Error ${err.message}`);
+    return res.status(400).send(`Webhook error: ${err.message}`);
   }
 
   if (event.type === 'checkout.session.completed')
