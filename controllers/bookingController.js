a2ab96b36499df03e5f2d2c3b5a98a29835d5a34
Paweł hamryszak <phamryszak91@gmail.com>
2023-02-22 15:59:37 +0100

    update url

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -12,10 +12,10 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
-    //   req.params.tourId
-    // }&user=${req.user.id}&price=${tour.price}`,
-    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
+    // success_url: `${req.protocol}://${req.get('host')}/my-tours`,
 
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
@@ -23,13 +23,13 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     mode: 'payment',
     line_items: [
       {
+        name: `${tour.name} Tour`,
+        description: tour.summary,
         quantity: 1,
         price_data: {
           currency: 'usd',
-          unit_amount: tour.price * 100,
+          amount: tour.price * 100,
           product_data: {
-            name: `${tour.name} Tour`,
-            description: tour.summary,
             images: [
               `${req.protocol}://${req.get('host')}/img/tours/${
                 tour.imageCover
