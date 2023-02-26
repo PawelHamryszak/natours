5c3db0fca61ebf60fa5b4573d5212efc4c0766ff
Paweł hamryszak <phamryszak91@gmail.com>
2023-02-26 21:37:28 +0100

    server update

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ process.on('unhandledRejection', (err) => {
 });
 
 process.on('SIGTERM', () => {
-  console.log('SIGTERM RECEIVED. Shutting down gracefully');
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
   server.close(() => {
     console.log('💥 Process terminated!');
   });
