d219d79d2ce6b5790737cd57b0ebdf85210e6713
Paweł hamryszak <phamryszak91@gmail.com>
2023-02-27 00:42:13 +0100

    booking controller update

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -43,16 +43,6 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   });
 });
 
-// exports.createBookingCheckout = catchAsync(async (req, res, next) => {
-//   // This is only TEMPORARY, because it's UNSECURE: everyone can make bookings without paying
-//   const { tour, user, price } = req.query;
-
-//   if (!tour || !price || !user) return next();
-//   await Booking.create({ tour, user, price });
-
-//   res.redirect(req.originalUrl.split('?')[0]);
-// });
-
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email }))._id;
