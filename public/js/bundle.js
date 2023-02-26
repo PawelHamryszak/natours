82fe15a1e07285ddba91e95772e678314345af40
Paweł hamryszak <phamryszak91@gmail.com>
2023-02-26 21:50:16 +0100

    bundle

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -37925,7 +37925,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "14792" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "12332" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
